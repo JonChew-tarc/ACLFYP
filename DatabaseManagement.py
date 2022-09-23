f53ceb814d87ef6bfaf17ceaa8d967c125e70d9a
JonChewChunWai <77605824+JonChew-tarc@users.noreply.github.com>
2022-09-23 15:29:42 +0800

    Add files via upload

---
+++ DatabaseManagement.py
@@ -0,0 +1,106 @@
+import json
+
+import DatabaseGateway
+
+class BaseAgentDatabaseManagement:
+
+    def __init__(self):
+        self.imageID = 0
+
+    def create_agentRecord(self, droneID, locationLatitude, locationLongitude, elevationLevel, batteryStatus, verticalSpeed, horizontalSpeed):
+
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+
+        query_string = "insert into droneagent (droneID, locationLatitude, locationLongitude, elevationLevel, batteryStatus, verticalSpeed, horizontalSpeed) values (%s, %s, %s, %s, %s, %s, %s)"
+        data = (droneID, locationLatitude, locationLongitude, elevationLevel, batteryStatus, verticalSpeed, horizontalSpeed)
+
+        mycursor.execute(query_string, data) 
+
+        mydb.commit()    
+
+        return self.read_records(mycursor.lastrowid)
+
+
+    def update_agentRecord(self, battery, locationLatitude, locationLongitude, elevation, horizontal, resource_id):
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        query_string = "update droneagent set batteryStatus = %s, locationLatitude = %s, locationLatitude = %s, verticalSpeed = %s, horizontalSpeed = %s where droneID = %s"
+
+        data = (battery, locationLatitude, locationLongitude, elevation, horizontal, resource_id)
+
+        mycursor.execute(query_string, data)  
+
+        return self.read_records(resource_id)
+
+    def delete_record(self, resource_id):
+
+        dg = DatabaseGateway.DatabaseGateway()
+
+        query_string = "delete from customers where customer_id = %s"
+
+        data = (resource_id,)
+
+        dg.db_execute(query_string, data) 
+
+        resp = ("Success",)
+
+        json_object = json.dumps(resp)
+
+        return json.dumps(json.loads(json_object), indent = 2)
+
+    def read_records(self, resource_id):
+
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        if resource_id == "" :
+            query_string = "select * from droneagent"
+        else:
+            query_string = "select * from droneagent where droneID = '" + str(resource_id) + "'"
+
+        resp = dg.db_query(query_string)      
+
+        return resp
+
+    def upload_image(self, resource_id):
+        self.imageID += 1
+
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        imageFormatter = "I" + self.imageID
+
+
+        query_string = "insert into capturedimages (imageID, imageByte) values (%s, %s)"
+        data = (imageFormatter, resource_id)
+
+        mycursor.execute(query_string, data) 
+
+        mydb.commit()    
+
+
+
+
+if __name__ == "__main__":
+    test = BaseAgentDatabaseManagement()
+    #test.create_agentRecord("user3@localhost", "70.4242", "24.2414", "40", 78, "0.241", "50.241")
+    #wow = test.read_records("")
+    #print (wow)
+    dg = DatabaseGateway.DatabaseGateway()
+    mydb = dg.db_connect()
+    mycursor = mydb.cursor()
+
+    #mycursor.execute("CREATE TABLE capturedimages (imageID VARCHAR(50), imageByte VARCHAR(255)) ENGINE = InnoDB")
+    mycursor.execute("SHOW TABLES")
+    for x in mycursor:
+        print(x)
+    
+
+
