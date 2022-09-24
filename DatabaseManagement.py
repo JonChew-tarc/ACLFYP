a4b717e76f3cd4b84f342d175c6601cc69b09a25
JonChewChunWai <77605824+JonChew-tarc@users.noreply.github.com>
2022-09-24 22:09:36 +0800

    Update DatabaseManagement.py

--- DatabaseManagement.py
+++ DatabaseManagement.py
@@ -37,13 +37,14 @@ class BaseAgentDatabaseManagement:
 
         return self.read_records(resource_id)
 
+
     def delete_record(self, resource_id):
 
         dg = DatabaseGateway.DatabaseGateway()
 
         query_string = "delete from customers where customer_id = %s"
 
-        data = (resource_id,)
+        data = (resource_id)
 
         dg.db_execute(query_string, data) 
 
@@ -75,15 +76,103 @@ class BaseAgentDatabaseManagement:
         mydb = dg.db_connect()
         mycursor = mydb.cursor()
 
-        imageFormatter = "I" + self.imageID
+        imageFormatter = "I" + str(self.imageID)
 
 
-        query_string = "insert into capturedimages (imageID, imageByte) values (%s, %s)"
+        query_string = "insert into captured_images (imageID, imageByte) values (%s, %s)"
         data = (imageFormatter, resource_id)
 
         mycursor.execute(query_string, data) 
 
         mydb.commit()    
+
+    def read_images(self, resource_id):
+
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        if resource_id == "" :
+            query_string = "select * from captured_images"
+        else:
+            query_string = "select * from captured_images where imageID = '" + str(resource_id) + "'"
+
+        resp = dg.db_query(query_string)      
+
+        return resp
+
+    def read_activetask(self, resource_id):
+
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        if resource_id == "" :
+            query_string = "select * from active_task"
+        else:
+            query_string = "select * from active_task where taskID = '" + str(resource_id) + "'"
+
+        resp = dg.db_query(query_string)      
+
+        return resp
+
+    def read_completetask(self, resource_id):
+
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        if resource_id == "" :
+            query_string = "select * from completed_task"
+        else:
+            query_string = "select * from completed_task where taskID = '" + str(resource_id) + "'"
+
+        resp = dg.db_query(query_string)      
+
+        return resp
+
+    def delete_activetask(self, task_id):
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        query_string = "DELETE FROM active_task WHERE taskID = '" + str(task_id) + "'"
+
+        mycursor.execute(query_string)
+
+        mydb.commit()
+
+        print(mycursor.rowcount, "record(s) deleted")
+
+    def add_activetask(self, task_id, task_name, task_type, generated_time, drone_id):
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        query_string = "INSERT INTO active_task (taskID, taskName, taskType, generatedTIme, droneID) values (%s, %s, %s, %s, %s)"
+
+        data = (task_id, task_name, task_type, generated_time, drone_id)
+
+
+        mycursor.execute(query_string, data)
+
+        mydb.commit()
+
+    def add_completedtask(self, task_id, task_name, task_type, drone_id):
+        dg = DatabaseGateway.DatabaseGateway()
+        mydb = dg.db_connect()
+        mycursor = mydb.cursor()
+
+        query_string = "INSERT INTO completed_task (taskID, taskName, taskType,  droneID) values (%s, %s, %s, %s)"
+
+        data = (task_id, task_name, task_type, drone_id)
+
+
+        mycursor.execute(query_string, data)
+
+        mydb.commit()
+
+        
 
 
 
@@ -91,13 +180,18 @@ class BaseAgentDatabaseManagement:
 if __name__ == "__main__":
     test = BaseAgentDatabaseManagement()
     #test.create_agentRecord("user3@localhost", "70.4242", "24.2414", "40", 78, "0.241", "50.241")
-    #wow = test.read_records("")
-    #print (wow)
+    wow = test.read_completetask("")
+    print (wow)
     dg = DatabaseGateway.DatabaseGateway()
     mydb = dg.db_connect()
     mycursor = mydb.cursor()
 
-    #mycursor.execute("CREATE TABLE capturedimages (imageID VARCHAR(50), imageByte VARCHAR(255)) ENGINE = InnoDB")
+    #mycursor.execute("CREATE TABLE captured_images (imageID VARCHAR(50), imageByte VARCHAR(1000)) ENGINE = InnoDB")
+    #mycursor.execute("CREATE TABLE active_task (taskID VARCHAR(50), taskName VARCHAR(50), taskType VARCHAR(50), generatedTime VARCHAR(50), droneID VARCHAR(50)) ENGINE = InnoDB")
+    #mycursor.execute("CREATE TABLE completed_task (taskID VARCHAR(50), taskName VARCHAR(50), taskType VARCHAR(50), droneID VARCHAR(50)) ENGINE = InnoDB")
+
+
+
     mycursor.execute("SHOW TABLES")
     for x in mycursor:
         print(x)
